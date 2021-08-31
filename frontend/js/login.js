684ae9e0f8ed94afd4e39ec5a88a838561197f31
Paul Wood <pmwood@pm.me>
2021-08-31 13:56:52 -0400

    feat(login-signup forms): form data is fetched, validated, and can now be sent to the server without reloading the page.

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,9 +1,51 @@
 function showLoginForm() {
-  document.getElementById("login-form").style.display = "block"
-  document.getElementById("signup-form").style.display = "none"
+  document.getElementById("login-section").style.display = "block"
+  document.getElementById("signup-section").style.display = "none"
 }
 
 function showSignupForm() {
-  document.getElementById("login-form").style.display = "none"
-  document.getElementById("signup-form").style.display = "block"
+  document.getElementById("login-section").style.display = "none"
+  document.getElementById("signup-section").style.display = "block"
 }
+
+function handleFormSubmit(event) {
+  console.log("Handing form submission")
+  const data = new FormData(event.target);
+  const formJson = Object.fromEntries(data.entries());
+  console.log(JSON.stringify(formJson));
+}
+
+function validateLoginCredentials(creds) {
+  return creds.username != "" && creds.password != "";
+}
+
+function tryLogin() {
+  const form = new FormData(document.getElementById("login-form"));
+  const formJson = Object.fromEntries(form.entries());
+  console.log("Attempting login: " + JSON.stringify(formJson, null, 2));
+
+  if (validateLoginCredentials(formJson)) {
+    console.log("Logging in...");
+    // TODO: Do login
+  }
+  else
+    console.log("Failed to login!");
+}
+
+function validateSignupCredentials(creds) {
+  return creds.username != "" && creds.password != "" && creds.password == creds.confirmedPassword;
+}
+
+function trySignup() {
+  const form = new FormData(document.getElementById("signup-form"));
+  const formJson = Object.fromEntries(form.entries());
+  console.log("Attempting signup: " + JSON.stringify(formJson, null, 2));
+
+  if (validateSignupCredentials(formJson)) {
+    console.log("Signing up...");
+    // TODO: Do signup
+  }
+  else
+    console.log("Failed to signup!");
+}
+
