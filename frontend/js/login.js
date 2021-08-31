6747d5040c98a7402db55b9eaf77be31d5a52049
Paul Wood <pmwood@pm.me>
2021-08-31 12:30:05 -0400

    feat(login-page): Created basic, unstyled login page (index.html)

---
+++ frontend/js/login.js
@@ -0,0 +1,9 @@
+function showLoginForm() {
+  document.getElementById("login-form").style.display = "block"
+  document.getElementById("signup-form").style.display = "none"
+}
+
+function showSignupForm() {
+  document.getElementById("login-form").style.display = "none"
+  document.getElementById("signup-form").style.display = "block"
+}
