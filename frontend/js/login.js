a3fcba32a29930335120c0947683e3fc210e3836
David Schneck <david.schneckster@hotmail.com>
2021-09-01 18:05:47 -0400

    jess

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,9 +1,11 @@
 function showLoginForm() {
+  const login = document.getElementById("option-button-login");
   document.getElementById("login-section").style.display = "block"
   document.getElementById("signup-section").style.display = "none"
 }
 
 function showSignupForm() {
+  const login = document.getElementById("option-button-signup");
   document.getElementById("login-section").style.display = "none"
   document.getElementById("signup-section").style.display = "block"
 }
@@ -12,39 +14,3 @@ function validateLoginCredentials(creds) {
   return creds.username != "" && creds.password != "";
 }
 
-function tryLogin() {
-  const form = new FormData(document.getElementById("login-form"));
-  const formJson = Object.fromEntries(form.entries());
-  console.log("Attempting login: " + JSON.stringify(formJson, null, 2));
-
-  if (validateLoginCredentials(formJson)) {
-    console.log("Logging in...");
-    // TODO: Do login
-  }
-  else
-    console.log("Failed to login!");
-}
-
-function validateSignupCredentials(creds) {
-  return creds.username != "" && creds.password != "" && creds.password == creds.confirmedPassword;
-}
-
-function trySignup() {
-  const form = new FormData(document.getElementById("signup-form"));
-  const formJson = Object.fromEntries(form.entries());
-  console.log("Attempting signup: " + JSON.stringify(formJson, null, 2));
-
-  if (validateSignupCredentials(formJson)) {
-    console.log("Signing up...");
-    // TODO: Do signup
-  }
-  else
-    console.log("Failed to signup!");
-}
-
-function tryGetPasswordHint() {
-  const form = new FormData(document.getElementById("login-form"));
-  const formJson = Object.fromEntries(form.entries());
-  console.log("Attempting to get password hint for username: " + formJson.username);
-  // TODO: Get password hint
-}
