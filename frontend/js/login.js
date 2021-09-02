fd893a02f8832c67d1a2d7111ff386f68bdb9fb4
David Schneck <david.schneckster@hotmail.com>
2021-09-02 16:20:58 -0400

    fix: implementing some cchanges

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,11 +1,22 @@
+const login = document.getElementById("option-button-login");
+const signup = document.getElementById("option-button-signup");
+
 function showLoginForm() {
-  const login = document.getElementById("option-button-login");
+  login.classList.remove('disabled');
+  login.className += " active";
+
+  signup.className += " disabled";
+
   document.getElementById("login-section").style.display = "block"
   document.getElementById("signup-section").style.display = "none"
 }
 
 function showSignupForm() {
-  const login = document.getElementById("option-button-signup");
+  signup.classList.remove('disabled');
+  signup.className += " active";
+
+  login.className += " disabled";
+
   document.getElementById("login-section").style.display = "none"
   document.getElementById("signup-section").style.display = "block"
 }
