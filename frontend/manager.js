4bde8faaa8f24d81574be6bd1893ad958a932ece
Paul Wood <pmwood@pm.me>
2021-09-08 13:21:12 -0400

    feat(dynamic-list): can add & remove list items

---
+++ frontend/manager.js
@@ -0,0 +1,74 @@
+class Contact {
+  constructor(
+    firstName, lastName, dob='', addr1='', addr2='', city='', 
+    state='', zip='', relation='', profession='', phoneno='', notes=''
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = dob;
+    this.addr1 = addr1;
+    this.addr2 = addr2;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.relation = relation;
+    this.profession = profession;
+    this.phoneno = phoneno;
+    this.notes = notes;
+  }
+
+  static fromObj(obj) {
+    return new Contact(
+      obj.firstName,
+      obj.lastName,
+      obj.dob,
+      obj.addr,
+      obj.addr,
+      obj.city,
+      obj.state, 
+      obj.zip, 
+      obj.relation,
+      obj.profession,
+      obj.phoneno,
+      obj.notes
+    );
+  }
+
+  static fromForm(id) {
+    // TODO
+  }
+}
+
+const listItemClass = 'list-group-item';
+const contactsList = document.getElementById('contactsList');
+console.log(contactsList);
+
+// let entry = document.createElement('li');
+// entry.textContent = "Generated Item";
+// entry.setAttribute('class', 'list-group-item');
+// contactsList.appendChild(entry);
+
+function contactListItemId(contact) {
+  // TODO: need to use some sort of unique ID
+  return `contact-li-${contact.firstName}-${contact.lastName}`;
+}
+
+function addContactListItem(contact) {
+  let li = document.createElement('li');
+  li.textContent = `${contact.firstName} ${contact.lastName}`;
+  li.setAttribute('class', listItemClass);
+  li.setAttribute('id', contactListItemId(contact));
+  contactsList.appendChild(li);
+}
+
+function removeContactListItem(contact) {
+  let li = document.getElementById(contactListItemId(contact));
+  contactsList.removeChild(li);
+}
+
+const paul = new Contact('Paul', 'Wood');
+const aaron = new Contact('Aaron', 'Wood');
+addContactListItem(paul);
+addContactListItem(aaron);
+
+removeContactListItem(paul);
