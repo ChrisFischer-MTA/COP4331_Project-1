c21c098cf46262f361a4c1757dfad2aec1c9f4a6
David Schneck <david.schneckster@hotmail.com>
2021-09-08 18:48:28 -0400

    feat: get object from forms

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -4,26 +4,66 @@ const signup = document.getElementById("option-button-signup");
 showLoginForm();
 
 function showLoginForm() {
-  login.classList.remove('disabled');
-  login.className += " active";
+	login.classList.remove('disabled');
+	login.className += " active";
 
-  signup.className += " disabled";
+	signup.className += " disabled";
 
-  document.getElementById("login-section").style.display = "block"
-  document.getElementById("signup-section").style.display = "none"
+	document.getElementById("login-section").style.display = "block"
+	document.getElementById("signup-section").style.display = "none"
 }
 
 function showSignupForm() {
-  signup.classList.remove('disabled');
-  signup.className += " active";
+	signup.classList.remove('disabled');
+	signup.className += " active";
 
-  login.className += " disabled";
+	login.className += " disabled";
 
-  document.getElementById("login-section").style.display = "none"
-  document.getElementById("signup-section").style.display = "block"
+	document.getElementById("login-section").style.display = "none"
+	document.getElementById("signup-section").style.display = "block"
 }
 
 function validateLoginCredentials(creds) {
-  return creds.username != "" && creds.password != "";
+	return creds.username != "" && creds.password != "";
+}
+
+function tryLogin() {
+	const myForm = document.getElementById("login-form");
+	const formData = new FormData(myForm);
+	//var jsonObj =  JSON.parse(JSON.stringify($('#formId').serializeObject()));
+
+	let object  = {};
+	formData.forEach((key, value) =>  {
+		object[key] = value;
+	});
+	console.log(object);
+
+	let json =  JSON.stringify(object);
+	console.log(json);
+
+	console.log(formData);
+	for (var pair of formData.entries()) {
+		console.log(pair);
+	}
+}
+
+function trySignup() {
+	const myForm = document.getElementById("signup-form");
+	const formData = new FormData(myForm);
+	//var jsonObj =  JSON.parse(JSON.stringify($('#formId').serializeObject()));
+
+	let object  = {};
+	formData.forEach((key, value) =>  {
+		object[key] = value;
+	});
+	console.log(object);
+
+	let json =  JSON.stringify(object);
+	console.log(json);
+
+	console.log(formData);
+	for (var pair of formData.entries()) {
+		console.log(pair);
+	}
 }
 
