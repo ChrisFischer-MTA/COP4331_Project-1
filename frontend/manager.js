38a6ecffac14e72fd80d5d240e2ef98768c8beb4
Paul Wood <pmwood@pm.me>
2021-09-09 15:04:17 -0400

    feat(manager-logic): laying groundwork for contact manager logic - still lots to do

--- frontend/manager.js
+++ frontend/manager.js
@@ -1,3 +1,170 @@
+class Result {
+  constructor() {
+    this.ok = null;
+    this.err = null;
+  }
+
+  static Ok(ok=null) {
+    let res = new Result();
+    res.ok = ok;
+    return res;
+  }
+
+  static Err(err) {
+    let res = new Result();
+    res.err = err;
+    return res;
+  }
+
+  on_err(func) {
+    if (this.err != null) 
+      return func(this.err);
+    else if (this.ok != null)
+      return this.ok;
+    else
+      throw 'Invalid Result object: no ok or err specified!';
+  }
+
+  on_ok(func) {
+    if (this.ok != null)
+      return func(this.ok);
+    else if (this.err != null)
+      return this.err;
+    else
+      throw 'Invalid Result object: no ok or err specified!';
+  }
+
+  then(ok_func, err_func) {
+    if (this.ok == null && this.err == null)
+      throw 'Invalid Result object: no ok or err specified!';
+
+    if (this.ok != null)
+      ok_func(this.ok);
+
+    if (this.err != null)
+      err_func(this.err);
+  }
+}
+
+class Manager {
+  constructor(listId, formId) {
+    this.listId = listId;
+    this.formId = formId;
+
+    // A dict where: 
+    //   key (string): contact html list item ID
+    //   value (Contact): the contact object
+    this.contacts = {};
+
+    // States:
+    //   nosel: when the page is first loaded, there isn't a selected contact to display
+    //   view:  a contact is being viewed (readonly)
+    //   edit:  a contact is being edited
+    this.STATES = {
+      NOSEL: 0,
+      VIEW: 1,
+      EDIT: 2,
+    }
+    this.state = STATES.NOSEL;
+    // this.httpConnection = '';
+    
+    // The currently selected contact list item ID (string)
+    this.selectedId = null;
+    this.selectedContact = null;
+  }
+
+  viewContact(htmlId) {
+    if (state == STATES.EDIT && hasChanges())
+      return new Promise((resolve, reject) => {
+        resolve(new Result.Err('A contact is already open with unsaved changes!'));
+      });
+
+    if (!hasContact(htmlId))
+      return new Promise((resolve, reject) => {
+        resolve(new Result.Err(`The contact ${html} does not exist!`));
+      });
+
+    let contact = this.contacts[htmlId];
+    selectedId = htmlId;
+    selectedContact = contact;
+
+    makeselectedId();
+    setFormData(contact); 
+    setFormReadOnly();
+
+    return new Promise((resolve, reject) =>{
+      resolve(new Result.Ok(contact));
+    });
+  }
+
+  editContact() {
+    if (state == STATES.EDIT)
+      return new Promise((resolve, reject) => {
+        resolve(new Result.Err('A contact is already being edited!'));
+      });
+
+    // selectedId = null should only happen when the page first loads but just adding
+    // for a sanity check.
+    if (state == STATES.NOSEL || selectedId == null)
+      return new Promise((resolve, reject) => {
+        resolve(new Result.Err('A contact is not currently selected!'));
+      });
+
+    editForm(); 
+     
+    return new Promise();
+  }
+
+  deleteContact(htmlId) {
+
+    return new Promise();
+  }
+
+  addContact() {
+
+    return new Promise();
+  }
+
+  hasContact(htmlId) {
+    return this.contacts.hasOwnProperty(htmlId);
+  }
+
+  search(searchStr) {
+    // TODO
+  }
+
+  hasChanges() {
+    // TODO
+    return true; 
+  }
+
+  // Up to caller to ensure proper state
+  makeSelection() {
+    // TODO
+  }
+
+  // Up to caller to ensure proper state
+  setFormData(contact) {
+    // TODO
+  }
+
+  // Up to caller to ensure proper state
+  setFormReadOnly() {
+    // TODO
+    state = STATES.VIEW;
+  }
+
+  // Up to caller to ensure proper state
+  editForm() {
+    // TODO
+    state = STATES.EDIT;
+  }
+
+  saveContact() {
+    // TODO
+  }
+}
+
 class Contact {
   constructor(
     firstName, lastName, dob='', addr1='', addr2='', city='', 
