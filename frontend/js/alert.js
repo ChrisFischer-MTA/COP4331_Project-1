de85c2b7bc430a490a7ed9d27455e6611594281d
Paul Wood <pmwood@pm.me>
2021-09-15 15:03:22 -0400

    feat(alerts): added dismiss button to alert

--- frontend/js/alert.js
+++ frontend/js/alert.js
@@ -5,11 +5,18 @@ export function createTimedAlert(context, message, time=5000, parent=document.bo
 
   alertDiv.innerHTML = `
   <div id="${alertId}" class="alert alert-danger cm-alert" role="alert">
-    ${message}
+    <p>${message}
+      <i class="bi bi-x close"></i>
+    </p>
   </div>`;
 
   console.log(parent);
   parent.appendChild(alertDiv);
+  document.getElementById(alertId).addEventListener('click', () => {
+    try {
+      parent.removeChild(alertDiv);
+    } catch {}
+  });
 
   setTimeout(
     () => {
