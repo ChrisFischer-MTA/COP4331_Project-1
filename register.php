2cea5f6e58fda7b0aee4a9c685d7355a59310d8b
Paul Wood <pmwood@pm.me>
2021-09-17 19:12:42 -0400

    fix(register): missing password hint field in sql cmd

--- register.php
+++ register.php
@@ -1,41 +1,53 @@
 <?php
-	$inData = getRequestInfo();
-
-	$firstname = $inData["firstname"];
-	$lastname = $inData["lastname"];
+  $inData = getRequestInfo();                                                                                   
+                                                                                                                
+  $firstname = $inData["firstname"];                                                                            
+  $lastname = $inData["lastname"];                                                                              
   $password = $inData["password"];
   $login = $inData["login"];#Why is there a login in register.php?
-	
-	$conn = new mysqli("localhost", "TheTester", "WeLoveCOP4331", "SmallProject");
-	if ($conn->connect_error)
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
-		$stmt->bind_param("ssss", $firstname, $lastname,$login,$password);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
+  $passwordHint = "";
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+  $conn = new mysqli("localhost", "TheTester", "WeLoveCOP4331", "SmallProject");
+  if ($conn->connect_error)
+  {
+    returnWithError( $conn->connect_error );
+  }
+  else
+  {
+    $stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password,PasswordHint) VALUES(?,?,?,?,?)");
+    if (!$stmt->bind_param("sssss", $firstname, $lastname,$login,$password,$passwordHint)) {
+      returnWithError( "Server Error: unable to bind SQL param. " . $stmt->error);
+      return;
+    }
+    if (!$stmt->execute()) { 
+      returnWithError( "Server Error: unable to execute SQL command. " . $stmt->error);
+      return;
+    }
+    $stmt->close();
+    $conn->close();
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
+    return returnWithNews(); 
+  }
 
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
+  function getRequestInfo()
+  {
+          return json_decode(file_get_contents('php://input'), true);
+  }
 
+  function sendResultInfoAsJson( $obj )
+  {
+          header('Content-type: application/json');
+          echo $obj;
+  }
+
+  function returnWithError( $err )
+  {
+          $retValue = '{"error":"' . $err . '"}';
+          sendResultInfoAsJson( $retValue );
+  }
+
+  function returnWithNews()
+  {
+          sendResultInfoAsJson('{"error":""}');
+  }
 ?>
