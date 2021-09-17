cc91832def3a8f22e8f7a306c7d396555f210991
Paul Wood <pmwood@pm.me>
2021-09-17 18:05:07 -0400

    fix: changed defalt contact ids
    Solely for testing since search.php doesnt work at the moment - contacts that are hard coded in the manager by default

--- frontend/js/manager.js
+++ frontend/js/manager.js
@@ -54,8 +54,8 @@ class ContactList extends Component {
 
     this.state = {
       elements: [
-        new Contact('Paul', 'Aye'),
-        new Contact('Ligma', 'Balls'),
+        new Contact('Paul', 'Wood', id=7),
+        new Contact('Ligma', 'Balls', id=67),
       ],
       topIsNewContact: false,
       // NOTE: maybe remove these
