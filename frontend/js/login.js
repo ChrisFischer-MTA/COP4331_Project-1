8b8acb13da5722678b2dcb918e681082188dedaf
David Schneck <david.schneckster@hotmail.com>
2021-09-20 14:22:45 -0400

    can't test locally, trying to see if login page works from the server

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,9 +1,12 @@
+import API from './api.js';
+
+const URL = "https://webapp.thegentlemengaming.com/LAMPAPI/login.php";
+
 const login_button = document.getElementById("option-button-login");
 const signup_button = document.getElementById("option-button-signup");
 
 const login_form  = document.getElementById("login-form")
 const signup_form  = document.getElementById("signup-form")
-
 
 function showLoginForm() {
 	login_button.classList.remove('disabled');
@@ -40,11 +43,33 @@ function tryLogin() {
 		object[key] = value;
 	});
 	console.log(object);
+	API.login(object['username'], object['password'])
 
 	/*
 	for (var pair of formData.entries()) {
 		console.log(pair);
 	}
+	*/
+	/*
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("POST", URL, true); 
+	xhttp.setRequestHeader("Content-Type", "application/json");
+	xhttp.onreadystatechange = function() {
+	   if (this.readyState == 4 && this.status == 200) {
+	        // Response
+			     var response = this.responseText;
+				    }
+					};
+					xhttp.send(JSON.stringify(object));
+	fetch(URL, {
+		method: "POST",
+		body: JSON.stringify(object),
+		headers: { 
+			"Content-type" : "application/json"
+		}
+	})
+		.then(response => response.json())
+		.then(returnData => console.log(returnData))
 	*/
 }
 
@@ -65,7 +90,10 @@ function trySignup() {
 	for (var pair of formData.entries()) {
 		console.log(pair);
 	}
+
 }
 
 // Main
 showLoginForm();
+
+export default login;
