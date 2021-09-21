9675f9be0b5c0e3b208423e08d9b68adf099c124
David Schneck <david.schneckster@hotmail.com>
2021-09-21 13:43:02 -0400

    test: added new fields for first name and last name

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,21 +1,19 @@
 import API from './api.js';
 
-const URL = "https://webapp.thegentlemengaming.com/LAMPAPI/login.php";
+const option_login_button = document.getElementById("option-button-login");
+const option_signup_button = document.getElementById("option-button-signup");
 
-const login_button = document.getElementById("option-button-login");
-const signup_button = document.getElementById("option-button-signup");
+option_login_button.addEventListener("click", showLoginForm);
+option_signup_button.addEventListener("click", showSignupForm);
 
-login_button.addEventListener("click", showLoginForm);
-signup_button.addEventListener("click", showSignupForm);
-
-const login_form  = document.getElementById("login-form")
-const signup_form  = document.getElementById("signup-form")
+const login_form  = document.getElementById("login-form");
+const signup_form  = document.getElementById("signup-form");
 
 function showLoginForm() {
-	login_button.classList.remove('disabled');
-	login_button.className += " active";
+	option_login_button.classList.remove('disabled');
+	option_login_button.className += " active";
 
-	signup_button.className += " disabled";
+	option_signup_button.className += " disabled";
 
 	login_form.style.display = "block"
 	signup_form.style.display = "none"
@@ -23,10 +21,10 @@ function showLoginForm() {
 }
 
 function showSignupForm() {
-	signup_button.classList.remove('disabled');
-	signup_button.className += " active";
+	option_signup_button.classList.remove('disabled');
+	option_signup_button.className += " active";
 
-	login_button.className += " disabled";
+	option_login_button.className += " disabled";
 
 	login_form.style.display = "none"
 	login_form.reset();
@@ -46,34 +44,9 @@ function tryLogin() {
 		object[key] = value;
 	});
 	console.log(object);
-	API.login(object['username'], object['password'])
-
-	/*
-	for (var pair of formData.entries()) {
-		console.log(pair);
+	if (validateLoginCredentials(object)) {
+		API.login(object['username'], object['password'])
 	}
-	*/
-	/*
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("POST", URL, true); 
-	xhttp.setRequestHeader("Content-Type", "application/json");
-	xhttp.onreadystatechange = function() {
-	   if (this.readyState == 4 && this.status == 200) {
-	        // Response
-			     var response = this.responseText;
-				    }
-					};
-					xhttp.send(JSON.stringify(object));
-	fetch(URL, {
-		method: "POST",
-		body: JSON.stringify(object),
-		headers: { 
-			"Content-type" : "application/json"
-		}
-	})
-		.then(response => response.json())
-		.then(returnData => console.log(returnData))
-	*/
 }
 
 function trySignup() {
@@ -86,14 +59,10 @@ function trySignup() {
 	});
 	console.log(object);
 
-	let json =  JSON.stringify(object);
-	console.log(json);
 
-	console.log(formData);
-	for (var pair of formData.entries()) {
-		console.log(pair);
+	if (validateLoginCredentials(object)) {
+		API.Register(object['firstname'], object['lastname'],object['username'], object['password'])
 	}
-
 }
 
 // Main
